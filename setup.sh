d616def138b9559a9483a567a21faa8377d50fb8
Arturo Rinaldi <arty.net2@gmail.com>
2018-06-16 01:38:30 +0200

    updating setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
+#!/bin/bash -x
+
 export GOPATH=$PWD
 
 go get github.com/go-errors/errors
 go get github.com/stretchr/testify
 go get github.com/jstemmer/go-junit-report
-go get arduino.cc/arduino-builder
+go get -u github.com/arduino/go-properties-map
+go get -u github.com/arduino/go-timeutils
+go get google.golang.org/grpc
+go get github.com/golang/protobuf/proto
+go get golang.org/x/net/context
+go get github.com/fsnotify/fsnotify
+go get github.com/arduino/arduino-builder
