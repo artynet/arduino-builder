103d4cc8cc0d126dad1e047213f78eb9eb5fca60
Arturo Rinaldi <arty.net2@gmail.com>
2019-04-01 01:42:12 +0200

    setting the right permission for the distrib/ folder after docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+DUSER=$(whoami)
+DUID=${UID}
+
 docker run --name abuilder --rm -v $(pwd):/root/arduino-builder \
 	-w /root/arduino-builder golang:latest \
-	bash -c "apt-get update && apt-get install -y tar bzip2 git zip && chmod +x *.sh && ./setup.sh && ./deploy.sh"
+	bash -c "useradd ${DUSER} -u ${DUID} && \
+	apt-get update && apt-get install -y tar bzip2 git zip && \
+	chmod +x *.sh && ./setup.sh && ./deploy.sh && \
+	chown ${DUSER}:${DUSER} -R distrib/ && \
+	chown ${DUSER}:${DUSER} -R arduino-builder/"
