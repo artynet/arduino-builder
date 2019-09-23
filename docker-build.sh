4f5bbc2d5a71b9a988377492269271b6a71dd1cf
Arturo Rinaldi <arty.net2@gmail.com>
2019-09-23 12:22:03 +0200

    fixing docker build for new release (1.4.7)

--- docker-build.sh
+++ docker-build.sh
@@ -9,4 +9,4 @@ docker run --name abuilder --rm -v $(pwd):/root/arduino-builder \
 	apt-get update && apt-get install -y tar bzip2 git zip && \
 	chmod +x *.sh && ./setup.sh && ./deploy.sh && \
 	chown ${DUSER}:${DUSER} -R distrib/ && \
-	chown ${DUSER}:${DUSER} -R arduino-builder/"
+	chown ${DUSER}:${DUSER} -R *.sh *.mod *.sum"
